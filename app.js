e6199995e93d221812faef2402cbdd54bc4eadf7
Harish S <115945412+Harishshanmugamm@users.noreply.github.com>
2023-11-08 19:42:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,249 +1,322 @@
 const express = require("express");
-const app=express();
-const path=require('path')
-const bodyParser = require('body-parser')
-const { Courses, Chapters, Pages, Users } = require('./models');
-const passport=require('passport')
-const connectEnsureLogin=require('connect-ensure-login')
-const session = require('express-session')
-const LocalStrategy=require('passport-local')
-const bcrypt= require('bcrypt');
-
-const saltRounds = 10;
+const app = express();
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const path = require("path");
+var bcrypt = require("bcrypt");
+var session = require("express-session");
+const LocalStrategy = require("passport-local");
+const passport = require("passport");
+const connectEnsurelogin = require("connect-ensure-login");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
 
+app.use(
+  session({
+    secret: "my-super-secret-key-54862547158632541257",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  }),
+);
 
-app.use(session({
-  secret:"my-super-secret-key-2121156448852896",
-  cookie:{
-    maxAge:24*60*60*1000
-  }
-}))
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(passport.initialize())
-app.use(passport.session())
-
-passport.use( new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password', 
-  roleField:'role' 
-}, async (email, password, done) => {
-  
-  try {
-    const user = await Users.findOne({ where: { email } });
-    const match = await bcrypt.compare(password, user.password);
-
-    if (!user) {
-      return done(null, false, { message: 'User not found' });
-    }
-
-    if (user.roles === 'student') {
-
-      if (match) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-    } else {
-      if (match) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-    }
-  } catch (error) {
-    return done(null,error);
-  }
-}));
-
-
-passport.serializeUser((user,done)=>{
-  console.log("Serializing student in session",user.id)
-  done(null, user.id)
-})
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await Users.findByPk(id);
-
-    if (!user) {
-      return done(new Error('Invalid user'));
-    }
-
-    if (user.roles !== 'student' && user.roles !== 'educator') {
-      return done(new Error('Invalid roles'));
-    }
-
-    done(null, user);
-  } catch (error) {
-    done(error);
-  }
-});
-
-
-
-
-/*const courseroute=require("./routes/courses")*/
-
-
-
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-
-
-
-app.get('/', (request, response)=>{
-    
-    response.render("index", {
-        title: "Learning Management System"
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      User.findOne({ where: { email: username } })
+        .then(async (user) => {
+          const result = await bcrypt.compare(password, user.password);
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Invalid password" });
+          }
+        })
+        .catch(() => {
+          return done(null, false, {
+            message: "Account is not exist for this mail",
+          });
         });
+    },
+  ),
+);
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session: ", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
+
+const { Courses, Chapters, Pages, User } = require("./models");
+
+
+const saltRounds = 10;
+
+app.get("/login", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Learning Management System",
+  });
+});
+
+app.get("/edsignup", (req, res) => {
+  res.render("educator", {
+    title: "Sign UP Educator",
+  });
+});
+
+app.post("/eusers", async (req, res) => {
+  const Hashed = await bcrypt.hash(req.body.password, saltRounds);
+  try {
+    const user = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: Hashed,
+      role: "educator",
+    });
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      const userRole = user.role;
+      res.redirect("/courses/new");
+    });
+  } catch (error) {
+    res.redirect("/");
+    console.log(error);
+  }
+});
+
+
+app.post("/susers", async (req, res) => {
+  const Hashed = await bcrypt.hash(req.body.password, saltRounds);
+  try {
+    const user = await User.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: Hashed,
+      role: "student",
+    });
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      res.redirect("/");
+    });
+  } catch (error) {
+    res.redirect("/");
+    console.log(error);
+  }
+});
+app.get("/educator/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: { email },
+    });
+
+    return res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
+});
+
+app.post(
+  "/educatorlogin",
+  passport.authenticate("local", {
+    failureRedirect: "/educator/login",
+  }),
+
+  (req, res) => {
+    console.log(req.user);
+    if (req.user.role === "educator") {
+      res.redirect("/courses/new");
+    } else {
+      res.redirect("/");
+    }
+  },
+);
+app.post("/studentlogin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: { email },
+    });
+    return res.redirect("/courses/new");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
+});
+
+app.post(
+  "/studentlogin",
+  passport.authenticate("local", {
+    failureRedirect: "/studentlogin",
+  }),
+  (req, res) => {
+    console.log(req.user);
+    if (req.user.role === "student") {
+      res.redirect("/courses/new");
+    } else {
+      res.redirect("/");
+    }
+  },
+);
+
+
+
+app.get("/signout", connectEnsurelogin.ensureLoggedIn(), (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.get("/courses/new", connectEnsurelogin.ensureLoggedIn(), (req, res) => {
+  res.render("courses");
+});
+
+app.post("/courses/new", connectEnsurelogin.ensureLoggedIn(), async (req, res) => {
+  try {
+    const title = req.body.title;
+    const course = await Courses.create({ title });
+    res.redirect(`/courses/${course.id}`);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get('/courses/:id',async (request,response)=>{
+  const courseId=request.params.id
+  const course=await Courses.findByPk(courseId)
+  response.render("chapter",{courseId,course})
   })
 
-  app.get('/courses',connectEnsureLogin.ensureLoggedIn(), async (req,resp)=>{
-    const courses = await Courses.findAll();
-    resp.render("dashboard", {
-      courses
+app.post("/courses/:id/chapters/new", async (request, response) => {
+  try {
+      const { title, description} = request.body;
+      const courseId=request.params.id
+      console.log(courseId)
+      if (!courseId) {
+        return res.status(400).json({ error: "Course ID is missing" });
+      }
+      const chapter = await Chapters.addChapter({
+        title,
+        description,
+        courseId,
       });
-  })
-  app.post('/courses',(req,resp)=>{
-    resp.redirect('/courses/new')
-  })
-
-  app.get('/home',(req,resp)=>{
-    
-    resp.render('home')
-  })
-
-  app.get('/courses/new', (request, response)=>{
-    
-    response.render("courses", {
-        title: "Learning Management System"
-        });
-  })
-  app.post('/courses/new', async (request, response) => {
-    const {name} = request.body;
-    try {
-      const course = await Courses.addnewcourse(
-        {name});
-    response.redirect(`/courses/${course.id}`);
-    } catch (err) {
-      console.error(err);
-      response.status(500).send('Unable to connect to server'); 
+      console.log(courseId);
+      console.log(title);
+      console.log(description);
+  
+      response.redirect(`/courses/${courseId}/chapters/${chapter.id}/page`);
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ error: "Internal Server Error" });
     }
   });
 
-  app.get('/courses/:id',async (request,response)=>{
-    const courseId=request.params.id
-    const course=await Courses.findByPk(courseId)
-    response.render("chapter",{course})
-    })
-  app.post('/courses/:id',async(request,response)=>{
-    try {
-      const course=await Courses.findByPk(courseId)
-      response.redirect(`/courses/${course.id}/chapters`);
-    } catch (err) {
-      console.error(err);
-      response.status(500).send('Unable to connect to server');
-  }})
-  
-  app.get('/courses/:id/chapters',async(request, response)=>{
-    response.render("newchapters")
-  })
-    app.post('/courses/:id/chapters', async (req, res) => {
-      const { title } = req.body;
-      const { id } = req.params;
-      const {description}=req.params;
-      
-      try {
-        const course = await Courses.findByPk(id);
-        const chapter = await Chapters.create({
-          title,description,
-          courseId: courses.id  
+  app.get(
+    `/courses/:courseId/chapters/:chapterId/page`,
+    connectEnsurelogin.ensureLoggedIn(),
+    async (req, res) => {
+      const chapterId = req.params.chapterId;
+      const courseId = req.params.courseId;
+      const chapter = await Chapters.findOne({ where: { id: chapterId } });
+      const course=await Courses.findOne({where:{id:courseId}})
+        res.render("page", {
+          chapter,
+          course,
+          courseId,
+          chapterId,
         });
-        
-        res.json(chapter);
-      } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
       }
-    });
- /* app.get('/chapters/new', async (request,response)=>{
-    response.render("newcourses")
-  })*/
+  );
 
+  app.post("/course/:courseId/chapter/:chapterId/page/new", async (req, res) => {
+    try {
+      const { title, content } = req.body;
+      const chapterId=req.params.chapterId
+
+      const page = await Pages.create({
+        title: title,
+        content: content,
+        chapterId: chapterId,
+      });
+      const courseId = req.params.courseId;
+
+      const pageId = page.id;
+      res.redirect(`/view/${courseId}/${chapterId}/${pageId}`);
   
-
-//Signup and login
-
-
-app.post('/educatorlogin',passport.authenticate('local',{failureRedirect:"/"},{successRedirect: '/courses'}),(request,response)=>{
-  response.redirect("/courses");
-})
-  app.get('/edsignup', (request, response)=>{
-    
-    response.render("./signup/educator", {
-        title: "Learning Management System"
-        });
-  })
-
-  app.post('/edsignup', async (request, response) => { 
-    const hashedPwd = await bcrypt.hash(request.body.password,saltRounds)
-    console.log(hashedPwd)
-    try {
-        const users = await Users.create({
-          firstName: request.body.firstName,
-          lastName:request.body.lastName,
-          email:request.body.email,
-          password:hashedPwd,
-          roles: 'educator'})
-          request.login(users,(err)=>{
-          if(err){
-            console.log(err)
-          }
-          response.redirect("/courses");
-        })
+      console.log(pageId);
     } catch (error) {
-        console.log(error);
+      console.error(error);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-});
+  });
 
- 
-
-  app.get('/signup', (request, response)=>{
-    
-    response.render("./signup/student", {
-        title: "Learning Management System"
-        });
-  })
-
-  app.post('/signup', async (request, response) => { 
-    try {
-        const users = await Users.create({
-          firstName: request.body.firstName,
-          lastName:request.body.lastName,
-          email:request.body.email,
-          password:request.body.password,
-          roles: 'student'})
-          request.login(users,(err)=>{
-            if(err){
-              console.log(err)
-            }
-            response.redirect("/home");
-          })
-       
-       
-    } catch (error) {
-        console.log(error);
-    }
-});
-
-  //signup and login
-
-
-module.exports=app;
+  app.get(
+    "/view/:courseId/:chapterId/:pageId",
+    connectEnsurelogin.ensureLoggedIn(),
+    async (req, res) => {
+      const courseId = req.params.courseId;
+      const chapterId = req.params.chapterId;
+      const pageId = req.params.pageId;
+  
+      try {
+        const page = await Pages.findByPk(pageId);
+  
+        if (!page) {
+          return res.status(404).send("Page not found");
+        }
+        const title = page.title;
+        const content = page.content;
+  
+        res.render("pageview", {
+          title,
+          content,
+          courseId,
+          chapterId,
+          });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    },
+  );
+  
+module.exports = app;
